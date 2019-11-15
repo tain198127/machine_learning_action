b9678bfffaa9d694c0ac635c84fff287c0e81f44
danebrown <tain198127@163.com>
2019-11-15 17:37:21 +0800

    增加香农熵计算公式

---
+++ Log.py
@@ -0,0 +1,33 @@
+import logging
+import time
+import os.path
+
+
+def init_log(file_name,ispersistent_file):
+    logging.basicConfig()
+    logger = logging.getLogger(file_name)
+    logger.setLevel(logging.DEBUG)  # Log等级总开关
+    rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)  # 输出到console的log等级的开关
+    # 第三步，定义handler的输出格式
+    # formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    formatter = logging.Formatter("%(message)s")
+
+    ch.setFormatter(formatter)
+
+    # 第四步，将logger添加到handler里面
+    logger.addHandler(ch)
+    if ispersistent_file :
+        log_path = os.path.dirname(os.path.realpath(file_name)) + '/log/'
+        log_name = log_path + rq + '.log'
+        logfile = log_name
+        fh = logging.FileHandler(logfile)
+        fh.setLevel(logging.ERROR)  # 输出到file的log等级的开关
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+    # 日志
+    logger.debug('this is a logger debug message')
+    return logger
---
+++ chapter3/ShannonEntropy.py
@@ -0,0 +1,36 @@
+import Log
+from math import log
+
+logger = Log.init_log(__name__, False)
+
+
+def create_date():
+    dataSet = [
+        [1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']
+    ]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def calcShannonEnt(dateSet):
+    numEntries = len(dateSet)
+    logger.debug("numEntries=%s", numEntries)
+    labelCounts = {}
+    for featVec in dateSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        # end if
+        labelCounts[currentLabel] += 1
+    # end for
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    #end for
+    logger.debug("shannon ent is %s",shannonEnt)
+    return shannonEnt
+
+
+mydata, labels = create_date();
+calcShannonEnt(mydata)
