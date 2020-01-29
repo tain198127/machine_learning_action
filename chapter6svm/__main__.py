5c29038dc143a84c377eb0e9bd3da9188b1b2692
danebrown <tain198127@163.com>
2020-01-29 14:56:28 +0800

    完善svm的算法

--- chapter6svm/__main__.py
+++ chapter6svm/__main__.py
@@ -9,14 +9,20 @@ import sys
 from mxnet import nd
 import torch as tc
 import colorful.colorful as cf
+
 logger = Log.init_log(__name__, False)
 base_path = os.path.dirname(os.path.abspath(__file__)) + "/.."
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base_path)
 
-
-
 if (__name__ == '__main__'):
     data, label = smo.load_data_set(current_path + '/testSet.txt')
     logger.debug(data)
-    print(data, label)
+    print(data)
+    print('-------------')
+    print(label)
+    print('-------------')
+    b, alphas = smo.smoSimple(data, label, 0.6, 0.001, 40)
+    print(b)
+    print('-------------')
+    print(alphas[alphas>0])
