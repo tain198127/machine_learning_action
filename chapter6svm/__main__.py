5fbc2c74a6a6828604ad2f2ddcc1eb6db4120c5e
danebrown <tain198127@163.com>
2020-01-30 21:44:28 +0800

    完善svm的算法

--- chapter6svm/__main__.py
+++ chapter6svm/__main__.py
@@ -15,26 +15,41 @@ logger = Log.init_log(__name__, False)
 base_path = os.path.dirname(os.path.abspath(__file__)) + "/.."
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base_path)
-
-if (__name__ == '__main__'):
+def simplesmo():
     data, label = smo.load_data_set(current_path + '/testSet.txt')
-    # logger.debug(data)
-    # print(data)
-    # print('-------------')
-    # print(label)
-    # print('-------------')
-    # b, alphas = smo.smoSimple(data, label, 0.6, 0.001, 40)
-    # print(b)
-    # print('-------------')
-    # print(alphas[alphas>0])
-
-    nb, nalpha = smoP(data, label,0.6,0.001,40)
+    logger.debug(data)
+    print(data)
+    print('-------------')
+    print(label)
+    print('-------------')
+    b, alphas = smo.smoSimple(data, label, 0.6, 0.001, 40)
+    print(b)
+    print('-------------')
+    print(alphas[alphas>0])
+def plattsmo():
+    data, label = smo.load_data_set(current_path + '/testSet.txt')
+    nb, nalpha = smoP(data, label, 0.6, 0.001, 40)
     print(nb)
     print(nalpha)
     ws = calcWs(nalpha, data, label)
     print(ws)
     dataMat = mat(data)
-    v = dataMat[1]*mat(ws)+nb
+    v = dataMat[1] * mat(ws) + nb
     print(v)
+def kernel():
+    testRbf(k1=0.7)
+def kernel_line():
+    testRbf(kmtd='lin')
+testmethod = {
+    '0':simplesmo,
+    '1':plattsmo,
+    '2':kernel,
+    '3':kernel_line
+}
+if (__name__ == '__main__'):
+    cmd = input("选择要测试的函数：0为simple，1为platt，2为kernel,3为kernel_lin\n")
+    method = testmethod.get(cmd)
+    method()
+
 
 
--- chapter6svm/platt_smo.py
+++ chapter6svm/platt_smo.py
@@ -41,6 +41,7 @@ def innerL(i, oS):
     """
 
     Ei = calcEk(oS, i)
+    #kkt条件
     if ((oS.labelMat[i] * Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or (
             (oS.labelMat[i] * Ei > oS.tol) and (oS.alphas[i] > 0)):
         j, Ej = selectJ(i, oS, Ei)  # this has been changed from selectJrand
@@ -104,9 +105,9 @@ def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):  # full Pl
     return oS.b, oS.alphas
 
 
-def testRbf(k1=1.3):
+def testRbf(k1=1.3,kmtd='rbf'):
     dataArr, labelArr = smo.load_data_set('testSetRBF.txt')
-    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1))  # C=200 important
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, (kmtd, k1))  # C=200 important
     datMat = mat(dataArr);
     labelMat = mat(labelArr).transpose()
     svInd = nonzero(alphas.A > 0)[0]
@@ -116,7 +117,7 @@ def testRbf(k1=1.3):
     m, n = shape(datMat)
     errorCount = 0
     for i in range(m):
-        kernelEval = kernelTrans(sVs, datMat[i, :], ('rbf', k1))
+        kernelEval = kernelTrans(sVs, datMat[i, :], (kmtd, k1))
         predict = kernelEval.T * multiply(labelSV, alphas[svInd]) + b
         if sign(predict) != sign(labelArr[i]): errorCount += 1
     print("the training error rate is: %f" % (float(errorCount) / m))
@@ -126,7 +127,7 @@ def testRbf(k1=1.3):
     labelMat = mat(labelArr).transpose()
     m, n = shape(datMat)
     for i in range(m):
-        kernelEval = kernelTrans(sVs, datMat[i, :], ('rbf', k1))
+        kernelEval = kernelTrans(sVs, datMat[i, :], (kmtd, k1))
         predict = kernelEval.T * multiply(labelSV, alphas[svInd]) + b
         if sign(predict) != sign(labelArr[i]): errorCount += 1
     print("the test error rate is: %f" % (float(errorCount) / m))
